3d2a7ab9b5ab65c5114723d8933c7d14203d7d02
Florian <96491040+Florian-Lankes@users.noreply.github.com>
2024-11-17 16:46:41 +0100

    Add files via upload

---
+++ src/main/java/de/HowTo/HowToApplication.java
@@ -0,0 +1,13 @@
+package de.HowTo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowToApplication.class, args);
+	}
+
+}
---
+++ src/main/java/de/HowTo/model/User.java
@@ -0,0 +1,5 @@
+package de.HowTo.model;
+
+public class User {
+	
+}
