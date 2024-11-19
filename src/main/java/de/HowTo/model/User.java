433e7191eaad864b6d91b773a93f1f02907f7f43
Florian-Lankes <lankesflorian@gmail.com>
2024-11-19 17:43:55 +0100

    added .gitignore and user model

--- src/main/java/de/HowTo/model/User.java
+++ src/main/java/de/HowTo/model/User.java
@@ -1,5 +1,51 @@
 package de.HowTo.model;
 
 public class User {
+
+	private String name;
+	private String surname;
+	private String username;
+	private String Email;
+	private String password;
+	
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 	
 }
