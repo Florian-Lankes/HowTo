2e30088753d192bfdc8f6b46b3c907fa7ca3e563
David <david.trieu@st.oth-regensburg.de>
2025-01-02 12:21:37 +0100

    Tutorial create html, channel html

--- src/main/java/com/HowTo/spring_boot_HowTo/controller/HomeController.java
+++ src/main/java/com/HowTo/spring_boot_HowTo/controller/HomeController.java
@@ -12,30 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-//	@GetMapping("/home")
-//	public String HowToHomeView() {
-//		
-//		return "home";
-//	}
-	
-	
-	//@RequestMapping(method = RequestMethod.GET, value = "/")
-	@GetMapping("/home")
-	public String HowToHomeView(Model model) {
-		
-		ArrayList<String> tutorial = new ArrayList<>();
-		
-		tutorial.add("1");
-		tutorial.add("2");
-		tutorial.add("3");
-		tutorial.add("4");
-		tutorial.add("5");
-		tutorial.add("6");
-		
-		model.addAttribute("tutorial", tutorial);
-		
-		return "home";
-	}
 	
 	@GetMapping("/")
 	public String HowToView(Model model) {
