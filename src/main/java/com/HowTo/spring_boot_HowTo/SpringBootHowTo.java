c36e65084173d96977199177c89d4d28463149bf
TrieuF <jonnytrieu@live.de>
2025-01-22 18:00:48 +0100

    random comment

--- src/main/java/com/HowTo/spring_boot_HowTo/SpringBootHowTo.java
+++ src/main/java/com/HowTo/spring_boot_HowTo/SpringBootHowTo.java
@@ -5,6 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication    
 public class SpringBootHowTo   {  
 	public static void main(String[] args)  {    
-		SpringApplication.run(SpringBootHowTo.class, args);    
+		SpringApplication.run(SpringBootHowTo.class, args);    //runs the main
 	}   
 }
